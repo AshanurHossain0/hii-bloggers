1bc17b1b371e55bea6cba777d24cd0c65f2b1a9b
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-09-22 14:32:21 +0530

    navbarCompleted

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import styles from "./featured.module.css"
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Ashanur here!</b> Share your knowledge and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image className={styles.image} src="/p1.jpeg" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa dicta</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea placeat voluptatem doloremque, labore a id voluptate ratione ullam alias perferendis, et at culpa consectetur tenetur ab quia voluptatibus! Necessitatibus, corporis. Facere dignissimos corrupti perferendis totam! Dolorem voluptatum saepe quam quo distinctio quidem accusamus aliquid magnam exercitationem quas, molestias quia magni dolores, fugiat sed voluptate. Similique neque ab dolorem. Ipsum, expedita.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,18 +10,18 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.social}>
         <Image src="/facebook.png" alt='facebook' width={24} height={24} />
-        <Image src="/instagram.png" alt='facebook' width={24} height={24} />
-        <Image src="/tiktok.png" alt='facebook' width={24} height={24} />
-        <Image src="/youtube.png" alt='facebook' width={24} height={24} />
+        <Image src="/instagram.png" alt='instagram' width={24} height={24} />
+        <Image src="/tiktok.png" alt='tiktok' width={24} height={24} />
+        <Image src="/youtube.png" alt='youtube' width={24} height={24} />
       </div>
       <div className={styles.logo}>
-        <Image src="/logo.png" alt="Hii Bloggers" width={120} height={45}/>
+        <img src="/logo.png" alt="Hii Bloggers"/>
       </div>
       <div className={styles.links}>
         <ThemeToggle/>
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link href="/" className={styles.link}>Homepage</Link>
+        <Link href="/" className={styles.link}>Contact</Link>
+        <Link href="/" className={styles.link}>About</Link>
         <AuthLinks/>
       </div>
     </div>
